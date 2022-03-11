c1cb9151ad4689fdaef353b0fad76147dc3ff479
George Okello <okoth603@gmail.com>
2022-03-11 17:21:17 +0300

    The C commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 28;
+
+	while (a < 38)
+	{
+		putchar(d);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
