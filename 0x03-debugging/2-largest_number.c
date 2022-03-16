ba98acf896644567e024eb5c78854c409c8bbc5c
GEORGE OKELLO <45455816+okellogeorgeokoth@users.noreply.github.com>
2022-03-16 19:59:58 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,9 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest=a;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	if (largest<b)
 	{
 		largest = b;
 	}
